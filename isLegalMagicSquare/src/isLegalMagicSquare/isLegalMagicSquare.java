fd6f5532b49cc57ff93f34adeab4e22f9ded6c03
X-Lai <X-Lai@users.noreply.github.com>
2018-03-09 20:10:49 +0800

    test

---
+++ isLegalMagicSquare/src/isLegalMagicSquare/isLegalMagicSquare.java
@@ -0,0 +1,15 @@
+package isLegalMagicSquare;
+
+import java.io.*;
+
+public class isLegalMagicSquare {
+
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+        char c = (char) br.read();
+        System.out.println(c);
+    }
+
+}
